bfd8864fca8505fdc4f22ac3830d886b464bfb7a
drashtirathod4 <drashti.r@simformsolutions.com>
2022-05-23 13:06:27 +0530

    feat: made some changes

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,9 @@ Bundler.require(*Rails.groups)
 
 module ShopifyApp
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
