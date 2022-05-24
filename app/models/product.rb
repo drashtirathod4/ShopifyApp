a639a7464e03849177a75d63bcd6894c9f329a1b
drashtirathod4 <drashti.r@simformsolutions.com>
2022-05-24 14:06:28 +0530

    feat: made some changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,7 +7,7 @@ class Product < ApplicationRecord
 
     after_create do
         product = Stripe::Product.create(name: name)
-        price = Stripe::Price.create(product: product.id, unit_amount: self.price, currency: "inr")
+        price = Stripe::Price.create(product: product, unit_amount: self.price, currency: "inr")
         update(stripe_product_id: product.id, stripe_price_id: price.id)
     end
 end
