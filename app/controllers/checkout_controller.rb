24fc2f6d994d23c1ce7115e32cca613006ca72b0
drashtirathod4 <drashti.r@simformsolutions.com>
2022-05-24 16:39:21 +0530

    fix: made some changes

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,6 +1,6 @@
 class CheckoutController < ApplicationController
-    skip_before_action :authenticate_user!
     skip_before_action :verify_authenticity_token
+    skip_before_action :authenticate_user!
 
     def create
         product = Product.find(params[:id])
