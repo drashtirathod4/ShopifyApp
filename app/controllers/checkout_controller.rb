d9459c055ad9c29ff43d21037aa0e60de6189655
drashtirathod4 <drashti.r@simformsolutions.com>
2022-05-24 14:16:13 +0530

    fix: skip devise authentication for webhooks controller

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,4 +1,7 @@
 class CheckoutController < ApplicationController
+    skip_before_action :authenticate_user!
+    skip_before_action :verify_authenticity_token
+
     def create
         product = Product.find(params[:id])
         @session = Stripe::Checkout::Session.create({
