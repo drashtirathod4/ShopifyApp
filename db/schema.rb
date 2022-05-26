9ffa9bd2704cf3c3134274ed7e3f666800670124
drashtirathod4 <drashti.r@simformsolutions.com>
2022-05-26 11:25:00 +0530

    feat: product can have prices with different currencies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_24_062245) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_26_053348) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_24_062245) do
     t.integer "sales_count", default: 0, null: false
     t.string "stripe_product_id"
     t.string "stripe_price_id"
+    t.string "currency", default: "inr"
   end
 
   create_table "users", force: :cascade do |t|
